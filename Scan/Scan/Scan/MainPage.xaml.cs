b17e3df4c50046a6be26dba7b037a6ad64318bb9
aminaibrahim <aminaibrahim790@gmail.com>
2017-11-18 23:11:29 +0530

    QR  SCAANING APP

---
+++ Scan/Scan/Scan/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace Scan
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            await Navigation.PushAsync(scanPage);
+            scanPage.OnScanResult += (result) =>
+            {
+                scanPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+            var options = new MobileBarcodeScanningOptions
+            {
+                AutoRotate = false,
+                UseFrontCameraIfAvailable = true,
+                TryHarder = true,
+                PossibleFormats = new List<ZXing.BarcodeFormat>
+                {
+                    ZXing.BarcodeFormat.EAN_8, ZXing.BarcodeFormat.EAN_13
+                }
+            };
+
+            //add options and customize page
+            scanPage = new ZXingScannerPage(options)
+            {
+               DefaultOverlayTopText = "Align the barcode within the frame",
+                DefaultOverlayBottomText = string.Empty,
+                DefaultOverlayShowFlashButton = true
+            };
+
+        }
+    }
+}

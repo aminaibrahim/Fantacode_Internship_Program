e66c258ad0243f69c45fdd75f30aad7889d01067
aminaibrahim <aminaibrahim790@gmail.com>
2017-12-09 13:09:07 +0530

    Parameter passing
    A simple app which pass parameters between 2 pages

---
+++ Diary/Diary/Diary/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Diary
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void NavigateButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1(today.Text,title.Text,note.Text));
+        }
+    }
+}
---
+++ Diary/Diary/Diary/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Diary
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 (string par1, string par2, string par3)
+		{
+			InitializeComponent ();
+            Da.Text = par1;
+            Head.Text = par2;
+            Content.Text = par3;
+		}
+	}
+}

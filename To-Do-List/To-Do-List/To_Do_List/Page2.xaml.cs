9766138fa2fcc3f2862891a5d0242076449555e4
aminaibrahim <aminaibrahim790@gmail.com>
2017-12-09 14:21:08 +0530

    To Do List
    Simple To do list app with ListView

---
+++ To-Do-List/To-Do-List/To_Do_List/Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace To_Do_List
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+            MainListView.ItemsSource = new List<Schedule>
+            {
+                new Schedule
+                {
+                    Date= "12/12/2017",
+                    Time =12,
+                    Note= "complete task1"
+                },
+                new Schedule
+                {
+                    Date= "13/12/2017",
+                    Time = 1,
+                    Note = "complete task2"
+                }
+            };
+		}
+	}
+}
---
+++ To-Do-List/To-Do-List/To_Do_List/Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace To_Do_List
+{
+   public class Schedule
+    {
+        public string Date { get; set; }
+        public int Time { get; set; }
+        public string Note { get; set;  }
+    }
+}

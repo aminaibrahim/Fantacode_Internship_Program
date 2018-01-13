fe7c7ab9509f12a8a94cef254f55a0c92f74579b
aminaibrahim <aminaibrahim790@gmail.com>
2018-01-13 23:05:04 +0530

    Task 7
    Task 7 Demo App

---
+++ DemoApp/DemoApp/DemoApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void Button(object sender,EventArgs e)
+        {
+            string welcome = Name.Text;
+            asd.Text = "Welcome\t " + welcome;
+        }
+        private async void Tap(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+	}
+}
---
+++ DemoApp/DemoApp/DemoApp/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+
+        public Page1()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
+}

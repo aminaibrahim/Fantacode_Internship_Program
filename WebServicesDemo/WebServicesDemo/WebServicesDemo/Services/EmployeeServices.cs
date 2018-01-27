2b2ed11cea237872c530345616120878aad05cef
aminaibrahim <aminaibrahim790@gmail.com>
2018-01-27 14:38:45 +0530

    Advanced MVVM and WebServices

---
+++ WebServicesDemo/WebServicesDemo/WebServicesDemo/Services/EmployeeServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebServicesDemo.Models;
+
+namespace WebServicesDemo.Services
+{
+    class EmployeeServices
+    {
+        public List<Employee> GetEmployee()
+
+        {
+            var list = new List<Employee>
+            {
+                new Employee
+                {
+                   Name = "Anu",
+                   Department = "Marketing"
+                },
+            };
+            return list;
+        }
+    }
+}
---
+++ WebServicesDemo/WebServicesDemo/WebServicesDemo/ViewModels/MainViewModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using WebServicesDemo.Models;
+using WebServicesDemo.Services;
+
+namespace WebServicesDemo.ViewModels
+{
+    public class MainViewModels : INotifyPropertyChanged
+    {
+        private List<Employee> _EmployeeList { get; set; }
+        public List<Employee> EmployeeList
+        {
+            get
+            {
+                return _EmployeeList;
+            }
+            set
+            {
+                _EmployeeList = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModels()
+        {
+            var employeeServices = new EmployeeServices();
+            EmployeeList = employeeServices.GetEmployee();
+        }
+
+        
+         public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+}

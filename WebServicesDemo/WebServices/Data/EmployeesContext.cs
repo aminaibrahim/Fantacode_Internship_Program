3e0b739f87fc7082bbf5cc9fe16ace248837d020
aminaibrahim <aminaibrahim790@gmail.com>
2018-01-27 15:47:29 +0530

    WebServices

---
+++ WebServicesDemo/WebServices/Data/EmployeesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebServicesDemo.Models;
+
+namespace WebServices.Models
+{
+    public class EmployeesContext : DbContext
+    {
+        public EmployeesContext (DbContextOptions<EmployeesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebServicesDemo.Models.Employee> Employee { get; set; }
+    }
+}
--- WebServicesDemo/WebServices/Models/Employee.cs
+++ WebServicesDemo/WebServices/Models/Employee.cs
@@ -6,7 +6,7 @@ namespace WebServicesDemo.Models
 {
     public class Employee
     {
-        
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Department { get; set; }
     }

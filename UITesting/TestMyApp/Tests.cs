190cd4f72b6c1db66255e6475a34967ee0f4c9a5
aminaibrahim <aminaibrahim790@gmail.com>
2018-02-16 01:23:28 +0530

    UI-Testing a Simple Calculator App

---
+++ UITesting/TestMyApp/Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TestMyApp
+{
+    [TestFixture(Platform.Android)]
+  //  [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+        [Test]
+        public void Add_6_to_10()
+        {
+            //Arrange
+            app.EnterText("num1","12");
+            app.EnterText("num2", "12");
+            //Act
+            app.Tap("AddTap");
+            //Assert
+            var appResult = app.Query("ResultId").First(result => result.Text == "24");
+            Assert.IsTrue(appResult != null, "label not displaying");
+        }
+        [Test]
+        public void Mul_6_to_10()
+        {
+            //Arrange
+            app.EnterText("num1", "2");
+            app.EnterText("num2", "2");
+            //Act
+            app.Tap("MulTap");
+            //Assert
+            var appResult = app.Query("ResultId").First(result => result.Text == "4");
+            Assert.IsTrue(appResult != null, "label not displaying");
+        }
+    }
+}
+
--- UITesting/UITesting/UITesting/MainPage.xaml.cs
+++ UITesting/UITesting/UITesting/MainPage.xaml.cs
@@ -9,9 +9,23 @@ namespace UITesting
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+            private async void AddButton_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(First.Text);
+            int num2 = Convert.ToInt32(Second.Text);
+            int res = num1 + num2;
+            Result.Text = Convert.ToString(res);
+        }
+        private async void MulButton_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(First.Text);
+            int num2 = Convert.ToInt32(Second.Text);
+            int res = num1 * num2;
+            Result.Text = Convert.ToString(res);
+        }
+    }
 }
